c45ec1fda1dd96b005940b6fc756c40ef153f684
IllusionLife <30587357+IllusionLife@users.noreply.github.com>
2021-11-22 23:48:24 +0200

    Update stackLogic.h

--- stackLogic.h
+++ stackLogic.h
@@ -1,1 +1,19 @@
+#ifndef CHIP_STACK_HEADER
+#define CHIP_STACK_HEADER
 
+struct SChipPosition {
+    int iXCoord;
+    int iYCoord;
+    int iChipValue;
+};
+
+typedef SChipPosition t_position;
+
+struct SChipTurn
+{
+    t_position SCurrentChip;
+    t_position SDestinationChip;
+    int iTurn;
+};
+
+#endif
